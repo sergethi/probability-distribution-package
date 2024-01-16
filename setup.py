e302a525de5ed122a5ea03e1282cc6e8858c240c
serge <nsergearistide@gmail.comgit config --global user.email nsergearistide@gmail.comgit config --global user.email nsergearistide@gmail.com>
2024-01-16 17:57:54 -0500

    completed gaussian distributions package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='srg_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['srg_distributions'],
+      author = 'sergethiPytest',
+      author_email = 'nsergearistide@gmail.com',
+      zip_safe=False)
---
+++ srg_distributions/README.md
@@ -0,0 +1,10 @@
+# probability distribution package
+
+
+Summary of the package
+
+# Files
+
+Explanation of the file in this package
+
+# Installation
